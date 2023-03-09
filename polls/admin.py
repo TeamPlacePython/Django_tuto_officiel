8dfdf488015d2d77ae70ce73210ed3272da20209
Laurent Jouron <jouronlaurent@hotmail.com>
2023-03-09 08:56:36 +0100

    end official tuto

--- polls/admin.py
+++ polls/admin.py
@@ -4,23 +4,23 @@ from polls.models import Choice, Question
 
 
 class ChoiceInline(admin.TabularInline):
-    pass
+    model = Choice
+    extra = 3
 
 
-# class ChoiceInline(admin.StackedInline):
-#     model = Choice
-#     extra = 3
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['question_text']}),
+        (
+            'Date information',
+            {'fields': ['pub_date'], 'classes': ['collapse']},
+        ),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date')
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
 
 
-# class QuestionAdmin(admin.ModelAdmin):
-#     fieldsets = [
-#         (None, {'fields': ['question_text']}),
-#         (
-#             'Date information',
-#             {'fields': ['pub_date'], 'classes': ['collapse']},
-#         ),
-#     ]
-#     inlines = [ChoiceInline]
-
-
-# admin.site.register(Question, QuestionAdmin)
+admin.site.register(Question, QuestionAdmin)
